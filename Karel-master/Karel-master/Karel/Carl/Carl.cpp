419b17cf28488ebb364959a09de002dbbcac45ff
Ross O'Brien <rossobrien2250@gmail.com>
2015-03-18 14:05:50 -0400

    Update 8.1
    Added the Carl.cpp file into the program

---
+++ Karel-master/Karel-master/Karel/Carl/Carl.cpp
@@ -0,0 +1,55 @@
+#include "Carl.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <conio.h>
+using namespace std;
+using namespace System::Windows::Forms;
+//#include <Application>
+
+int sugardaddy::move()
+{
+	return 5;
+}
+void sugardaddy::turnleft()
+{
+	return;
+}
+void sugardaddy::turnoff()
+{
+	Application::Exit();
+}
+void sugardaddy::putbeeper()
+{
+
+}
+void sugardaddy::pickbeeper()
+{
+
+}
+
+string sugardaddy::getFile(int start, int end){
+	ifstream a_file;
+	string strInput;
+	a_file.open("Swag.txt");
+
+	for (int i = start; i < end; i++){
+		getline(a_file, strInput);
+	}
+	return strInput;
+}
+
+string sugardaddy::parsing(int a, int b, int c, int d){
+	string str = getFile(c, d);
+	string start = str.substr(a, b);
+	string end = str.substr(b);
+
+	return start;
+}
+
+int sugardaddy::getNum(int e, int f, int g, int h){
+	string stri = parsing(e, f, g, h);
+	int num = std::stoi(stri);
+
+	return num;
+}
